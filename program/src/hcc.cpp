a55dc3f427e7588ea3f768bbcb181438527ff618
Arnaud Lemaire <dev@norad.fr>
2012-06-03 19:51:34 +0200

    up

--- program/src/hcc.cpp
+++ program/src/hcc.cpp
@@ -1,55 +1,54 @@
-#include "hcc.h"
-#include "../lib/arduino/Arduino.h" // for serial
-#include "driver/network.h"
-#include "settings/settings.h"
-#include "client/client.h"
-#include "config/config-manager.h"
+#include "../lib/Ethernet/Ethernet.h"
 
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network.
+// gateway and subnet are optional:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,42, 245);
+IPAddress gateway(192, 168, 42, 1);
+IPAddress subnet(255, 255, 255, 0);
 
-char *definitionError;
-char *criticalProblem_p;
-uint8_t needReboot = false;
-t_notification *notification = 0;
+// telnet defaults to port 23
+EthernetServer server(23);
+boolean gotAMessage = false; // whether or not you got a message from the client yet
 
-void DEBUG_P(const prog_char *progmem) {
-    char c;
-    while ((c = pgm_read_byte(progmem++))) {
-        DEBUG_PRINT(c);
-    }
-    DEBUG_PRINTLN();
+void setup() {
+  // initialize the ethernet device
+  Ethernet.begin(mac, ip, gateway, subnet);
+  // start listening for clients
+  server.begin();
+  // open the serial port
+  Serial.begin(9600);
 }
 
-void DEBUG_E(const char *eeprom) {
-    char c;
-    while ((c = eeprom_read_byte((uint8_t *)eeprom++))) {
-        DEBUG_PRINT(c);
+void loop() {
+  // wait for a new client:
+  EthernetClient client = server.available();
+
+  // when the client sends the first byte, say hello:
+  if (client) {
+    if (!gotAMessage) {
+      Serial.println("We have a new client");
+      client.println("Hello, client!");
+      gotAMessage = true;
     }
-    DEBUG_PRINTLN();
+
+    // read the bytes incoming from the client:
+    char thisChar = client.read();
+    // echo the bytes back to the client:
+    server.write(thisChar);
+    // echo the bytes to the server as well:
+    Serial.print(thisChar);
+  }
 }
+
 
 int main(void) {
     init(); // load init of arduino
 
-#ifdef DEBUG
-    delay(3000);
-    Serial.begin(9600);
-#endif
-
-    definitionError = configCheck();
-    settingsLoad();
-    networkSetup();
-    if (!definitionError) {
-        pinInit();
-    }
-
-    clientBoardNotify(BOARD_NOTIFY_BOOT);
+    setup();
 
     while (1) {
-        networkManage();
-        if (!definitionError) {
-            pinCheckChange();
-        }
+        loop();
     }
-
-    return 0;
 }
