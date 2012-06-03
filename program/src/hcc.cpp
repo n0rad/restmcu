ccc1b9ddf7873d1b9d3584802635957b1eee29ac
Arnaud Lemaire <dev@norad.fr>
2012-06-03 20:06:45 +0200

    up

--- program/src/hcc.cpp
+++ program/src/hcc.cpp
@@ -1,45 +1,18 @@
-#include "../lib/Ethernet/Ethernet.h"
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network.
-// gateway and subnet are optional:
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,42, 245);
-IPAddress gateway(192, 168, 42, 1);
-IPAddress subnet(255, 255, 255, 0);
-
-// telnet defaults to port 23
-EthernetServer server(23);
-boolean gotAMessage = false; // whether or not you got a message from the client yet
+#include <Arduino.h>
 
 void setup() {
-  // initialize the ethernet device
-  Ethernet.begin(mac, ip, gateway, subnet);
-  // start listening for clients
-  server.begin();
-  // open the serial port
-  Serial.begin(9600);
+
+    pinMode(9, OUTPUT);
+
 }
 
 void loop() {
-  // wait for a new client:
-  EthernetClient client = server.available();
 
-  // when the client sends the first byte, say hello:
-  if (client) {
-    if (!gotAMessage) {
-      Serial.println("We have a new client");
-      client.println("Hello, client!");
-      gotAMessage = true;
-    }
+    digitalWrite(9, HIGH);   // set the LED on
+    delay(1000);              // wait for a second
+    digitalWrite(9, LOW);    // set the LED off
+    delay(1000);              // wait for a second
 
-    // read the bytes incoming from the client:
-    char thisChar = client.read();
-    // echo the bytes back to the client:
-    server.write(thisChar);
-    // echo the bytes to the server as well:
-    Serial.print(thisChar);
-  }
 }
 
 
