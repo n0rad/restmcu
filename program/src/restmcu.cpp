d530dd31d580c04a97c16b99bcf4f6abf5290447
Arnaud Lemaire <dev@norad.fr>
2012-06-09 21:43:59 +0200

    makefile take care of arduino board type (standard, mega)

--- program/src/restmcu.cpp
+++ program/src/restmcu.cpp
@@ -1,18 +1,18 @@
 /*
   Web Server
-
+ 
  A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
-
+ using an Arduino Wiznet Ethernet shield. 
+ 
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
  * Analog inputs attached to pins A0 through A5 (optional)
-
+ 
  created 18 Dec 2009
  by David A. Mellis
  modified 9 Apr 2012
  by Tom Igoe
-
+ 
  */
 
 #include <SPI.h>
@@ -20,12 +20,12 @@
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
-byte mac[] = {
+byte mac[] = { 
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,42, 245);
 
 // Initialize the Ethernet server library
-// with the IP address and port you want to use
+// with the IP address and port you want to use 
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
@@ -76,7 +76,7 @@ void loop() {
             client.print(analogChannel);
             client.print(" is ");
             client.print(sensorReading);
-            client.println("<br />");
+            client.println("<br />");       
           }
           client.println("</html>");
           break;
@@ -84,7 +84,7 @@ void loop() {
         if (c == '\n') {
           // you're starting a new line
           currentLineIsBlank = true;
-        }
+        } 
         else if (c != '\r') {
           // you've gotten a character on the current line
           currentLineIsBlank = false;
@@ -99,14 +99,3 @@ void loop() {
   }
 }
 
-
-
-int main(void) {
-    init(); // load init of arduino
-
-    setup();
-
-    while (1) {
-        loop();
-    }
-}
